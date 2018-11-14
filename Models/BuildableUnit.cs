d51d677d9ff4eea4c1ab24b2424c8087ac3b7098
Hugo MORPHEE <hugo.morphee@c17e.com>
2018-11-14 10:23:46 +0100

    Step 2 & 3 : Add Class BuildableUnit + pull/push branch

---
+++ Models/BuildableUnit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotFactory.Models
+{
+    public abstract class BuildableUnit
+    {
+        public double BuildTime { get; set; }
+
+        public BuildableUnit()
+        {
+            this.BuildTime = 5;
+        }
+
+        public BuildableUnit (double buildTime = 5)
+        {
+            this.BuildTime = buildTime;
+        }
+    }
+}
